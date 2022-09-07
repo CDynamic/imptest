4058fa3bb0f883ecc92c09f25bcf3b97a6ed9e21
José Gregorio Silva <josesilva@conceptualdynamic.com>
2022-09-07 16:43:18 -0400

    test

--- report_xlsx/report/report_partner_xlsx.py
+++ report_xlsx/report/report_partner_xlsx.py
@@ -13,4 +13,4 @@ class PartnerXlsx(models.AbstractModel):
         sheet = workbook.add_worksheet("Report")
         for i, obj in enumerate(partners):
             bold = workbook.add_format({"bold": True})
-            sheet.write(i, 0, obj.name, bold)
+            sheet.write(i, 0, obj.name, bold) 
